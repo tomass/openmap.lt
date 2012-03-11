a69b21c1f516ad3aa7534d46dc402807d64d954a
ramunasd <ieskok@ramuno.lt>
2012-03-11 14:22:40 +0200

    - replace control structure

--- public/poi.php
+++ public/poi.php
@@ -175,17 +175,16 @@ function fetch_poi($left, $top, $right, $bottom, $p_type, Poi_Format_Abstract $f
             $p_type = 'theatre|cinema|arts|library';
             break;
     }
-    $types = explode("|", $p_type);
+    $types = explode('|', $p_type);
     if (!is_array($types) or count($types) == 0) {
         debug("ERROR: Incorrect type parameter");
         die;
     }
     $id = 0;
-    $i = 0;
-    while ($i < count($types)) {
-        debug("processing type=" . $types[$i]);
+    foreach($types as $type) {
+        debug("processing type=" . $type);
 
-        switch($types[$i]) {
+        switch($type) {
             case 'history':
                 $filter = "historic is not null and historic not in ('monument', 'memorial')";
                 $default_title = 'Istorinė vieta';
@@ -330,7 +329,7 @@ function fetch_poi($left, $top, $right, $bottom, $p_type, Poi_Format_Abstract $f
         while ($row = pg_fetch_assoc($res)) {
             debug("lat:" . $row['lat'] . ", lon:" . $row['lon'] . ", tags:" . $row['name']);
             $row['tp'] = $tp;
-            $row['type'] = $types[$i];
+            $row['type'] = $type;
             // process title & description
             assemble_title($row, $default_title);
             assemble_description($row);
@@ -338,7 +337,6 @@ function fetch_poi($left, $top, $right, $bottom, $p_type, Poi_Format_Abstract $f
             $format->addRow($row);
             $id++;
         }
-        $i++;
     } // while loop through all type values
 } // fetch_poi
 
